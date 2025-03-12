4c5c7031e55090ffa516beb0df7d6fb145dad524
Melek Şayan <147090632+meleksayan@users.noreply.github.com>
2025-03-12 15:52:48 +0300

    Add files via upload

---
+++ factorial_calculation.R
@@ -0,0 +1,10 @@
+############### multiplication of numbers starting from 1 to itself ############3
+
+number_3 <- 5
+factoriall <- 1
+
+for( x in 2:number_3){
+  factoriall <- factoriall * x 
+  
+}
+print(factoriall)
